a0ff3a3288ede954de4d915f90c24aca49ed994d
Jordan <148658758+jalex-op@users.noreply.github.com>
2023-10-31 17:45:58 -0230

    passing code for: add, subtract, sum, multiply, and power

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
-};
+// The goal for this exercise is to create a calculator that does the following:
+// add, subtract, get the sum, multiply, get the power, and find the factorial
 
-const subtract = function() {
-	
-};
+// In order to do this please fill out each function with your solution. Make sure to return the value so you can test it in Jest! To see the expected value
+// take a look at the spec file that houses the Jest test cases.
 
-const sum = function() {
-	
-};
 
-const multiply = function() {
+// should be able to input as many numbers as you want, including with decimals
+
+const add = function(a, b) {
+  return (a + b);
 
 };
 
-const power = function() {
-	
+const subtract = function(a, b) {
+	return (a - b);
 };
+
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);          // basically says the same as a for loop. ", 0" means start the sum with 0
+};                                                                      // in case an empty array is input
+
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current);
+};
+
+const power = function(a, b) {
+	return Math.pow(a, b);
+};
+
+
 
 const factorial = function() {
 	
 };
+
+
+// npm test calculator.spec.js
 
 // Do not edit below this line
 module.exports = {
@@ -31,3 +46,4 @@ module.exports = {
   power,
   factorial
 };
+
