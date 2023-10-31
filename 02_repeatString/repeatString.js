2a7c493e4204f955a8a3a87dc7a8299ba7f2d80a
jordan <jalexander9383+1@gmail.com>
2023-10-31 10:47:00 -0230

    fix code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(stringText, times) {
+    let result = '';
+    for (let i = 0; i < times; i++) {
+        result = result + stringText;   // could also do result += stringText, is same thing but less clear code
+    }
+    return result;
 };
 
 // Do not edit below this line
